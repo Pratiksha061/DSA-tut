2e762fd06f1ecc85a9462849b7c52e5060420e31
Pratiksha061 <129214804+Pratiksha061@users.noreply.github.com>
2023-03-28 23:50:01 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Sorting{
+    public static int SelectionSort(int arr[]){
+      
+        for(int i=0; i<arr.length-1; i++){
+              int minPos=i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos]> arr[j]){
+                    minPos=j;
+            }
+        }
+        //swap
+        int temp= arr[minPos];
+        arr[minPos]=arr[i];
+        arr[i]=temp;
+    }
+    return 0;
+    }
+     public static void PrintArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String args[]){
+        int arr[]={5,4,3,1,2};
+        SelectionSort(arr);
+        PrintArr(arr);
+        
+    }
+}
